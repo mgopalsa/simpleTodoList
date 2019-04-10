db83198908e2e1c1ba3f96f6e2fc25d6372ea0d9
mgopalsa <42105370+mgopalsa@users.noreply.github.com>
2019-04-10 17:26:05 +1000

    Simple TodoList app

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app V1.0.0. It is part of React learning objective </p>
+    </React.Fragment>
+  )
+}
+
+export default About;
